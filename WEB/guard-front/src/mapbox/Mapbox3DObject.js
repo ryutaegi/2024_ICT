303a07965a5e60bb53517066708b3a74d4536efd
ryutaegi <gaonnurirtg@hanmail.net>
2024-08-16 13:57:50 +0000

    FEAT : Create map and object using react-three-map

--- WEB/guard-front/src/mapbox/Mapbox3DObject.js
+++ WEB/guard-front/src/mapbox/Mapbox3DObject.js
@@ -1,114 +1,33 @@
-import 'mapbox-gl/dist/mapbox-gl.css';
-import React, { useRef, useEffect, useState } from 'react';
-import { Canvas, useFrame, useThree } from '@react-three/fiber';
-import Map, { useMap } from 'react-map-gl';
-import mapboxgl from 'mapbox-gl';
-import * as THREE from 'three';
-
-mapboxgl.accessToken = 'pk.eyJ1IjoidGFleW91IiwiYSI6ImNsZHY2ajVkejA4MGszdm5vaWpvdG41Nm0ifQ.05ZauoKkriS9v4sp6ozTAA';
-
-const Box = ({ position }) => {
-  const mesh = useRef();
-
-  useFrame(() => {
-    if (mesh.current) {
-      mesh.current.rotation.x += 0.01;
-      mesh.current.rotation.y += 0.01;
-      mesh.current.position.set(position[0], position[1], position[2]);
-    }
-  });
-
-  return (
-    <mesh ref={mesh}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={'orange'} />
-    </mesh>
-  );
-};
-
-const ThreeOverlay = ({ map, position }) => {
-  const { scene, camera, gl } = useThree();
-
-  useEffect(() => {
-    if (!map) return;
-
-    const light = new THREE.DirectionalLight(0xffffff, 1);
-    light.position.set(0, 0, 10).normalize();
-    scene.add(light);
-
-    const render = () => {
-      const { lng, lat } = map.getCenter();
-      const mercator = mapboxgl.MercatorCoordinate.fromLngLat({ lng, lat }, 0);
-
-      camera.position.set(mercator.x, mercator.y, 2);
-      camera.lookAt(mercator.x, mercator.y, 0);
-
-      gl.autoClear = false;
-      gl.clearDepth();
-      gl.render(scene, camera);
-
-      map.triggerRepaint();
-    };
-
-    map.on('render', render);
-    render();
-
-    return () => {
-      map.off('render', render);
-    };
-  }, [map, scene, camera, gl]);
-
-  return null;
-};
+import "maplibre-gl/dist/maplibre-gl.css"
+import { createRoot } from 'react-dom/client'
+import React, { useRef, useState } from 'react'
+import { useFrame } from "@react-three/fiber"
+import Map from "react-map-gl/maplibre"
+import { Canvas } from "react-three-map/maplibre" 
+// import { Canvas } from "react-three-map" // if you are using MapBox
+import { Box } from '@react-three/drei';
 
 const Mapbox3DObject = () => {
-  const mapRef = useRef();
-  const [position, setPosition] = useState([0, 0, 0]);
-
-  useEffect(() => {
-    if (mapRef.current) {
-      const map = mapRef.current.getMap();
-
-      const updatePosition = () => {
-        const { lng, lat } = map.getCenter();
-        const mercator = mapboxgl.MercatorCoordinate.fromLngLat({ lng, lat }, 0);
-        setPosition([mercator.x, mercator.y, mercator.z || 0]);
-      };
-
-      map.on('move', updatePosition);
-      updatePosition();
-
-      return () => {
-        map.off('move', updatePosition);
-      };
-    }
-  }, []);
-
-  return (
-    <div style={{ height: '100vh', position: 'relative' }}>
-      <Map
-        ref={mapRef}
-        initialViewState={{
-          longitude: -74.006,
-          latitude: 40.7128,
-          zoom: 15,
-        }}
-        style={{ width: '100%', height: '100%' }}
-        mapStyle="mapbox://styles/mapbox/streets-v11"
-        mapboxAccessToken={mapboxgl.accessToken}
+  return <Map
+    antialias
+    initialViewState={{
+      latitude: 37.57796,
+      longitude: 126.97658,
+      zoom: 13,
+      pitch: 60
+    }}
+    mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
+  >
+    <Canvas latitude={37.57796} longitude={126.97658}>
+      <hemisphereLight
+        args={["#ffffff", "#60666C"]}
+        position={[1, 4.5, 3]}
       />
-      {mapRef.current && (
-        <Canvas
-          style={{ position: 'absolute', top: 0, left: 0, pointerEvents: 'none' }}
-          camera={{ position: [0, 0, 2], fov: 75 }}
-        >
-          <ThreeOverlay map={mapRef.current.getMap()} position={position} />
-          <Box position={position} />
-        </Canvas>
-      )}
-    </div>
-  );
-};
-
+      <object3D scale={500}>
+        <Box position={[-1.2, 1, 0]} />
+        <Box position={[1.2, 1, 0]} />
+      </object3D>
+    </Canvas>
+  </Map>
+}
 export default Mapbox3DObject;
-
--- WEB/guard-front/src/mapbox/test.js
+++ WEB/guard-front/src/mapbox/test.js
@@ -1,1 +1,114 @@
-i
+import 'mapbox-gl/dist/mapbox-gl.css';
+import React, { useRef, useEffect, useState } from 'react';
+import { Canvas, useFrame, useThree } from '@react-three/fiber';
+import Map, { useMap } from 'react-map-gl';
+import mapboxgl from 'mapbox-gl';
+import * as THREE from 'three';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoidGFleW91IiwiYSI6ImNsZHY2ajVkejA4MGszdm5vaWpvdG41Nm0ifQ.05ZauoKkriS9v4sp6ozTAA';
+
+const Box = ({ position }) => {
+  const mesh = useRef();
+
+  useFrame(() => {
+    if (mesh.current) {
+      mesh.current.rotation.x += 0.01;
+      mesh.current.rotation.y += 0.01;
+      mesh.current.position.set(position[0], position[1], position[2]);
+    }
+  });
+
+  return (
+    <mesh ref={mesh}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={'orange'} />
+    </mesh>
+  );
+};
+
+const ThreeOverlay = ({ map, position }) => {
+  const { scene, camera, gl } = useThree();
+
+  useEffect(() => {
+    if (!map) return;
+
+    const light = new THREE.DirectionalLight(0xffffff, 1);
+    light.position.set(0, 0, 10).normalize();
+    scene.add(light);
+
+    const render = () => {
+      const { lng, lat } = map.getCenter();
+      const mercator = mapboxgl.MercatorCoordinate.fromLngLat({ lng, lat }, 0);
+
+      camera.position.set(mercator.x, mercator.y, 2);
+      camera.lookAt(mercator.x, mercator.y, 0);
+
+      gl.autoClear = false;
+      gl.clearDepth();
+      gl.render(scene, camera);
+
+      map.triggerRepaint();
+    };
+
+    map.on('render', render);
+    render();
+
+    return () => {
+      map.off('render', render);
+    };
+  }, [map, scene, camera, gl]);
+
+  return null;
+};
+
+const Mapbox3DObject = () => {
+  const mapRef = useRef();
+  const [position, setPosition] = useState([0, 0, 0]);
+
+  useEffect(() => {
+    if (mapRef.current) {
+      const map = mapRef.current.getMap();
+
+      const updatePosition = () => {
+        const { lng, lat } = map.getCenter();
+        const mercator = mapboxgl.MercatorCoordinate.fromLngLat({ lng, lat }, 0);
+        setPosition([mercator.x, mercator.y, mercator.z || 0]);
+      };
+
+      map.on('move', updatePosition);
+      updatePosition();
+
+      return () => {
+        map.off('move', updatePosition);
+      };
+    }
+  }, []);
+
+  return (
+    <div style={{ height: '100vh', position: 'relative' }}>
+      <Map
+        ref={mapRef}
+        initialViewState={{
+          longitude: -74.006,
+          latitude: 40.7128,
+          zoom: 15,
+        }}
+        style={{ width: '100%', height: '100%' }}
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+        mapboxAccessToken={mapboxgl.accessToken}
+      />
+      {mapRef.current && (
+        <Canvas
+          style={{ position: 'absolute', top: 0, left: 0, pointerEvents: 'none' }}
+          camera={{ position: [0, 0, 2], fov: 75 }}
+        >
+          <ThreeOverlay map={mapRef.current.getMap()} position={position} />
+          <Box position={position} />
+        </Canvas>
+      )}
+    </div>
+  );
+};
+
+export default Mapbox3DObject;
+
